1217b71129a18246e3976295f44ce9f2c0960370
TestyBacon <74383994+TestyBacon@users.noreply.github.com>
2021-10-29 21:35:20 -0700

    Removed / Added Comments

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,19 @@ void main() {
   runApp(const MyApp());
 }
 
+// Main driver thing
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'THE VOID',
+
+      // Theme for whole app
+
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        // primarySwatch: Colors.white,
         primaryColor: const Color(0xff9ad5f4),
         scaffoldBackgroundColor: const Color(0xff487B9a),
         textTheme: const TextTheme(
@@ -34,17 +28,10 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Home page
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -52,26 +39,19 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+// State for the Home Page
+
 class _MyHomePageState extends State<MyHomePage> {
-  // void clearText(){
-  //   TextField.clear()
-  // }
   var fieldControl = TextEditingController();
   var currentSaved = "";
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: [
+          // Settings button
           IconButton(
             onPressed: () {
               Navigator.push(
@@ -87,31 +67,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            // Text(
-            //   'HELLO WORLD',
-            // ),
-            // Text(
-            //   '$_counter',
-            //   style: Theme.of(context).textTheme.headline4,
             Text(
               'Enter troubles here',
               style: Theme.of(context).textTheme.bodyText1,
@@ -135,11 +93,12 @@ class _MyHomePageState extends State<MyHomePage> {
                   border: UnderlineInputBorder(
                       borderSide: BorderSide(color: Colors.white))),
             ),
+            // Row of Buttons
             Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              // Button 1
               TextButton(
                 child: const Text('SUBMIT'),
                 onPressed: () {
-                  // debugPrint(fieldControl.text);
                   currentSaved = fieldControl.text;
                   fieldControl.text = "";
                 },
@@ -149,6 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   textStyle: const TextStyle(fontSize: 20),
                 ),
               ),
+              // Button 2
               TextButton(
                 child: const Text('CLEAR'),
                 onPressed: () {
@@ -160,6 +120,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   textStyle: const TextStyle(fontSize: 20),
                 ),
               ),
+              // Button 3
               TextButton(
                 child: const Text('REPLACE'),
                 onPressed: () {
@@ -175,8 +136,10 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      // Vault button
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          // Vault screen (temporarily void screen)
           Navigator.push(
               context,
               MaterialPageRoute(
@@ -188,12 +151,13 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         tooltip: 'Vault',
         child: const Icon(Icons.savings_rounded),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
 
 // Settings Page
+// Theres literally nothing on here
 
 class Settings extends StatelessWidget {
   const Settings({Key? key}) : super(key: key);
@@ -209,6 +173,7 @@ class Settings extends StatelessWidget {
         body: Center(
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // Go Back
           ElevatedButton(
             onPressed: () {
               Navigator.pop(context);
@@ -218,6 +183,8 @@ class Settings extends StatelessWidget {
         ])));
   }
 }
+
+// Vault screen
 
 class VaultView extends StatelessWidget {
   const VaultView({Key? key, required this.savedText}) : super(key: key);
@@ -245,6 +212,8 @@ class VaultView extends StatelessWidget {
     );
   }
 }
+
+// Vault state
 
 class SendToVoid extends StatefulWidget {
   const SendToVoid({Key? key, required this.savedText}) : super(key: key);
@@ -300,6 +269,14 @@ class _SendToVoidState extends State<SendToVoid> {
 }
 
 // IGNORE THE REST
+
+// Seriously ignore this
+
+// But don't delete it
+
+// It's for animation reference
+
+// Stop reading this Jacob
 
 class StaggerAnimation extends StatelessWidget {
   StaggerAnimation({Key? key, required this.controller})
