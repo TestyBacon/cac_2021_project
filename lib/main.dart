a4f488f1b7b0d29cf0bd951327530d0a2d780661
TestyBacon <74383994+TestyBacon@users.noreply.github.com>
2021-10-30 18:23:35 -0700

    Send to void button, the void is black now

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: const Color(0xff487B9a),
         textTheme: const TextTheme(
           bodyText1: TextStyle(fontSize: 25.0),
+          bodyText2:
+              TextStyle(fontSize: 50.0, color: Colors.white), // Void text
         ),
       ),
       home: const MyHomePage(title: 'THE VOID'),
@@ -123,9 +125,17 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
               // Button 3
               TextButton(
-                child: const Text('REPLACE'),
+                child: const Text('SEND TO VOID'),
                 onPressed: () {
-                  fieldControl.text = currentSaved;
+                  Text voidText = Text(fieldControl.text,
+                      style: Theme.of(context).textTheme.bodyText2);
+                  currentSaved = '';
+                  fieldControl.text = '';
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              SendToVoid(savedText: voidText)));
                 },
                 style: TextButton.styleFrom(
                   padding: const EdgeInsets.all(16.0),
@@ -144,11 +154,8 @@ class _MyHomePageState extends State<MyHomePage> {
           Navigator.push(
               context,
               MaterialPageRoute(
-                  builder: (context) => SendToVoid(
-                          savedText: Text(
-                        currentSaved,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ))));
+                  builder: (context) =>
+                      VaultView(savedText: Text(currentSaved))));
         },
         tooltip: 'Vault',
         child: const Icon(Icons.savings_rounded),
@@ -224,7 +231,7 @@ class SendToVoid extends StatefulWidget {
 class _SendToVoidState extends State<SendToVoid> {
   double rotation = 0.0;
   double opacity = 1.0;
-  Duration duration = const Duration(milliseconds: 3000);
+  Duration duration = const Duration(milliseconds: 3500);
   Curve curve = Curves.easeInBack;
   void change() {
     setState(() {
@@ -240,8 +247,9 @@ class _SendToVoidState extends State<SendToVoid> {
     });
 
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: const Text("Vault"),
+        title: const Text("THE VOID"),
       ),
       body: Center(
           child: Column(
@@ -249,7 +257,6 @@ class _SendToVoidState extends State<SendToVoid> {
         children: [
           AnimatedContainer(
             padding: const EdgeInsets.all(8.0),
-            height: 100.0,
             alignment: Alignment.center,
             child: AnimatedOpacity(
               opacity: opacity,
@@ -266,216 +273,8 @@ class _SendToVoidState extends State<SendToVoid> {
             duration: duration,
             curve: curve,
           ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Text("Go Back!"),
-          )
         ],
       )),
-    );
-  }
-}
-
-// IGNORE THE REST
-
-// Seriously ignore this
-
-// But don't delete it yet
-
-// It's for animation reference
-
-// Stop reading this Jacob
-
-// Just animation Reference
-class StaggerAnimation extends StatelessWidget {
-  StaggerAnimation({Key? key, required this.controller})
-      :
-
-        // Each animation defined here transforms its value during the subset
-        // of the controller's duration defined by the animation's interval.
-        // For example the opacity animation transforms its value during
-        // the first 10% of the controller's duration.
-
-        opacity = Tween<double>(
-          begin: 0.0,
-          end: 1.0,
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: const Interval(
-              0.0,
-              0.100,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        width = Tween<double>(
-          begin: 50.0,
-          end: 150.0,
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: const Interval(
-              0.125,
-              0.250,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        height = Tween<double>(begin: 50.0, end: 150.0).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: const Interval(
-              0.250,
-              0.375,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        padding = EdgeInsetsTween(
-          begin: const EdgeInsets.only(bottom: 16.0),
-          end: const EdgeInsets.only(bottom: 75.0),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: const Interval(
-              0.250,
-              0.375,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        borderRadius = BorderRadiusTween(
-          begin: BorderRadius.circular(4.0),
-          end: BorderRadius.circular(75.0),
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: const Interval(
-              0.375,
-              0.500,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        color = ColorTween(
-          begin: Colors.indigo[100],
-          end: Colors.orange[400],
-        ).animate(
-          CurvedAnimation(
-            parent: controller,
-            curve: const Interval(
-              0.500,
-              0.750,
-              curve: Curves.ease,
-            ),
-          ),
-        ),
-        super(key: key);
-
-  final Animation<double> controller;
-  final Animation<double> opacity;
-  final Animation<double> width;
-  final Animation<double> height;
-  final Animation<EdgeInsets> padding;
-  final Animation<BorderRadius?> borderRadius;
-  final Animation<Color?> color;
-
-  // This function is called each time the controller "ticks" a new frame.
-  // When it runs, all of the animation's values will have been
-  // updated to reflect the controller's current value.
-  Widget _buildAnimation(BuildContext context, Widget? child) {
-    return Container(
-      padding: padding.value,
-      alignment: Alignment.bottomCenter,
-      child: Opacity(
-        opacity: opacity.value,
-        child: Container(
-          width: width.value,
-          height: height.value,
-          decoration: BoxDecoration(
-            color: color.value,
-            border: Border.all(
-              color: Colors.indigo[300]!,
-              width: 3.0,
-            ),
-            borderRadius: borderRadius.value,
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      builder: _buildAnimation,
-      animation: controller,
-    );
-  }
-}
-
-class StaggerDemo extends StatefulWidget {
-  const StaggerDemo({Key? key}) : super(key: key);
-
-  @override
-  _StaggerDemoState createState() => _StaggerDemoState();
-}
-
-class _StaggerDemoState extends State<StaggerDemo>
-    with TickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _controller = AnimationController(
-        duration: const Duration(milliseconds: 2000), vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  Future<void> _playAnimation() async {
-    try {
-      await _controller.forward().orCancel;
-      await _controller.reverse().orCancel;
-    } on TickerCanceled {
-      // the animation got canceled, probably because we were disposed
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Staggered Animation'),
-      ),
-      body: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () {
-          _playAnimation();
-        },
-        child: Center(
-          child: Container(
-            width: 300.0,
-            height: 300.0,
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.1),
-              border: Border.all(
-                color: Colors.black.withOpacity(0.5),
-              ),
-            ),
-            child: StaggerAnimation(controller: _controller.view),
-          ),
-        ),
-      ),
     );
   }
 }
